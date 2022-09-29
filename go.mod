afed84e42564519678486bb5fad5f6850b00ea74
shelestinaa <azuzu1488@gmail.com>
2022-09-29 16:42:46 +0300

    added some important details and logger as known as logrus pkg

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/shelestinaa/justparser
 
 go 1.19
 
-require github.com/BurntSushi/toml v1.2.0 // indirect
+require (
+	github.com/BurntSushi/toml v1.2.0 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+)
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -1,17 +1,39 @@
 package apiserver
 
+import "github.com/sirupsen/logrus"
+
 type APIServer struct {
 	config *Config
+	logger *logrus.Logger
 }
 
 func New(config *Config) *APIServer {
 
 	return &APIServer{
 		config: config,
+		logger: logrus.New(),
 	}
 }
 
 func (s *APIServer) Start() error {
+
+	err := s.configureLogger()
+	if err != nil {
+		return err
+	}
+
+	s.logger.Info("starting api server is going on as fast as u are running to obtain white drugs, sport")
+	return nil
+}
+
+func (s *APIServer) configureLogger() error {
+	level, err := logrus.ParseLevel(s.config.LogLevel)
+
+	if err != nil {
+		return err
+	}
+
+	s.logger.SetLevel(level)
 
 	return nil
 }

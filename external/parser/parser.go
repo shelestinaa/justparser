8364232a01a34f9b87011116c90ec9e7bfb12f0f
shelestinaa <azuzu1488@gmail.com>
2022-10-13 12:42:37 +0300

    fixed some errors add route for GET /get-file

--- external/parser/parser.go
+++ external/parser/parser.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"fmt"
 	db2 "github.com/shelestinaa/justparser/external/db"
+	"github.com/sirupsen/logrus"
 	"github.com/xuri/excelize/v2"
 	"go.mongodb.org/mongo-driver/bson"
 	"io/ioutil"
@@ -11,23 +12,24 @@ import (
 	"time"
 )
 
-func Parse(buf []byte, filename string) error {
+func Parse(buf []byte, filename string) {
 
 	filenameToSave := "files/saved-file.xlsx"
 	err := ioutil.WriteFile(filenameToSave, buf, 0777)
 	if err != nil {
-		return err
+		logrus.Fatalf(err.Error())
 	}
 
 	var reportFile *excelize.File
+
 	reportFile, err = excelize.OpenFile(filenameToSave)
 	if err != nil {
-		return err
+		logrus.Fatalf(err.Error())
 	}
 
 	defer func() {
 		if err := reportFile.Close(); err != nil {
-			fmt.Printf(err.Error())
+			logrus.Fatalf(err.Error())
 		}
 	}()
 
@@ -35,58 +37,52 @@ func Parse(buf []byte, filename string) error {
 
 	db, err := db2.NewClient(ctx, "localhost", "27017", "", "", "justparser", "")
 	if err != nil {
-		return err
+		logrus.Fatalf(err.Error())
 	}
 
 	sheetMap := reportFile.GetSheetMap()
 
-	//var result []string
+	err = db.Collection(filename).Drop(ctx)
+	if err != nil {
+		logrus.Fatalf(err.Error())
+	}
+
+	err = db.CreateCollection(ctx, filename)
+	if err != nil {
+		logrus.Fatalf(err.Error())
+	}
+
 	for _, sheetTitle := range sheetMap {
 
-		var rowsToPersist []string
+		var rowsToPersist []byte
 
-		err := db.Collection(filename).Drop(ctx)
+		rows, err := reportFile.GetRows(sheetTitle)
 		if err != nil {
-			return err
+			logrus.Fatalf(err.Error())
 		}
-
-		err = db.CreateCollection(ctx, filename)
-		if err != nil {
-			return err
-		}
-
-		rows, _ := reportFile.GetRows(sheetTitle)
 
 		for _, row := range rows {
 			implodedStringRow := strings.Join(row, ",")
-			//encodedRow, arr, err := bson.MarshalValue(implodedStringRow)
-			//if err != nil {
-			//	return err
-			//}
-			rowsToPersist = append(rowsToPersist, implodedStringRow)
+			rowsToPersist = append(rowsToPersist, []byte(implodedStringRow)...)
 
 		}
 		type Document struct {
-			title string
-			data  []byte
+			Title string
+			Data  []byte
 		}
 
-		newDocument := &Document{sheetTitle, []byte(rowsToPersist)}
-		documentToPersist, _ := bson.Marshal(newDocument)
+		newDocument := &Document{sheetTitle, rowsToPersist}
+
+		documentToPersist, err := bson.Marshal(&newDocument)
+		if err != nil {
+			logrus.Fatalf(err.Error())
+		}
+
 		insertResult, err := db.Collection(filename).InsertOne(ctx, documentToPersist)
 		if err != nil {
-			return err
+			logrus.Fatalf(err.Error())
 		}
+
 		fmt.Printf(fmt.Sprintf("File have been parsed and persisted successfully. Its ID is: %s ", insertResult.InsertedID))
 	}
-
-	// Тут логика парсера, которую я не придумал
-	//todo: узнать про монго, мне кажется коллекции там не просто так, вместо таблиц
-
-	/*
-		Двигаемся слева направо по столбцам *1, как только теряем значение, конец
-		Затем двигаемся по буквам
-	*/
-
-	return nil
 }
--- internal/app/apiserver/apiserver.go
+++ internal/app/apiserver/apiserver.go
@@ -2,19 +2,30 @@ package apiserver
 
 import (
 	"bytes"
+	"encoding/json"
 	"fmt"
 	"github.com/gorilla/mux"
+	db2 "github.com/shelestinaa/justparser/external/db"
 	"github.com/shelestinaa/justparser/external/parser"
 	"github.com/sirupsen/logrus"
+	"go.mongodb.org/mongo-driver/bson"
+	"golang.org/x/net/context"
 	"io"
+	"mime/multipart"
 	"net/http"
 	"strings"
+	"time"
 )
 
 type APIServer struct {
 	config *Config
 	logger *logrus.Logger
 	router *mux.Router
+}
+
+type response struct {
+	code string
+	data []byte
 }
 
 func New(config *Config) *APIServer {
@@ -55,8 +66,8 @@ func (s *APIServer) configureLogger() error {
 func (s *APIServer) configureRouter() {
 	s.router.HandleFunc("/hello", s.handleHello())
 	s.router.HandleFunc("/parse", s.handleParse())
-	s.router.HandleFunc("/get-list", s.handleGetFilesList())
-	s.router.HandleFunc("/get-parsed-file", s.handleGetFileById())
+	s.router.HandleFunc("/get-list", s.handleGetCollections())
+	s.router.HandleFunc("/get-parsed-file", s.handleGetCollectionByName())
 }
 
 func (s *APIServer) handleHello() http.HandlerFunc {
@@ -67,36 +78,128 @@ func (s *APIServer) handleHello() http.HandlerFunc {
 
 func (s *APIServer) handleParse() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		r.ParseMultipartForm(32 << 20)
+
+		err := r.ParseMultipartForm(32 << 20)
+		if err != nil {
+			logrus.Fatalf(err.Error())
+		}
+
 		var buf bytes.Buffer
+
 		file, header, err := r.FormFile("key")
 		if err != nil {
 			logrus.Fatalf(err.Error())
-			io.WriteString(w, err.Error())
-			panic(err)
 		}
-		defer file.Close()
+
+		defer func(file multipart.File) {
+			err := file.Close()
+			if err != nil {
+				logrus.Fatalf(err.Error())
+			}
+		}(file)
+
 		name := strings.Split(header.Filename, ".")
-		fmt.Printf("File name %s\n", name[0])
+		filename := name[0]
+		fmt.Printf("File name %s\n", filename)
 
-		//тут забираю данные в свой буфер
 		io.Copy(&buf, file)
-		fmt.Printf("File %s has been copied successfully\n", name[0])
 
-		//тут вызываем парселку, отдаём в неё buf
-		parser.Parse(buf.Bytes(), name[0])
+		fmt.Printf("File %s has been copied successfully\n", filename)
+
+		parser.Parse(buf.Bytes(), filename)
+		fmt.Printf("File %s has been parsed successfully\n", filename)
 
 	}
 }
 
-func (s *APIServer) handleGetFilesList() http.HandlerFunc {
+func (s *APIServer) handleGetCollections() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "БАРЭФ")
+		ctx, _ := context.WithTimeout(context.Background(), time.Minute)
+
+		db, err := db2.NewClient(ctx, "localhost", "27017", "", "", "justparser", "")
+		if err != nil {
+			logrus.Fatalf(err.Error())
+		}
+
+		collectionNames, err := db.ListCollectionNames(ctx, bson.D{{}})
+		if err != nil {
+			logrus.Fatalf(err.Error())
+		}
+
+		_, err = io.WriteString(w, strings.Join(collectionNames, ", "))
+		if err != nil {
+			logrus.Fatalf(err.Error())
+		}
 	}
 }
 
-func (s *APIServer) handleGetFileById() http.HandlerFunc {
+func (s *APIServer) handleGetCollectionByName() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		io.WriteString(w, "SALAM")
+		ctx, _ := context.WithTimeout(context.Background(), time.Minute)
+
+		db, err := db2.NewClient(ctx, "localhost", "27017", "", "", "justparser", "")
+		if err != nil {
+			logrus.Fatalf(err.Error())
+		}
+
+		filename := r.URL.Query().Get("name")
+		currentCollection := db.Collection(filename)
+
+		cursor, err := currentCollection.Find(ctx, bson.D{{}})
+		if err != nil {
+			logrus.Fatalf(err.Error())
+		}
+
+		type Document struct {
+			Title string
+			Data  []byte
+		}
+
+		type TransformedDocument struct {
+			Title string
+			Data  string
+		}
+
+		type Response struct {
+			Code       int
+			Filename   string
+			ParsedFile []TransformedDocument
+		}
+
+		var documents []Document
+		var transformedDocuments []TransformedDocument
+
+		err = cursor.All(ctx, &documents)
+
+		for _, document := range documents {
+
+			var documentData string
+			documentData = string(document.Data)
+
+			if err != nil {
+				logrus.Fatalf(err.Error())
+			}
+
+			transformedDocuments = append(transformedDocuments, TransformedDocument{
+				Title: document.Title,
+				Data:  documentData,
+			})
+		}
+
+		response := Response{
+			Code:       200,
+			Filename:   filename,
+			ParsedFile: transformedDocuments,
+		}
+
+		marshalled, err := json.Marshal(response)
+		if err != nil {
+			logrus.Fatalf(err.Error())
+		}
+
+		_, err = w.Write(marshalled)
+		if err != nil {
+			logrus.Fatalf(err.Error())
+		}
 	}
 }
